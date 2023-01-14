62e0fbb42cace32987603560bef8ec19b98e30d5
ibhaka <iihalilkalkan@gmail.com>
2023-01-14 15:04:57 +0300

    Initial Commit

---
+++ CollectionView_in_TableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView_in_TableViewCell
+//
+//  Created by İbrahim Halil Kalkan on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
